70954bdead48eb0a0e19b579e774d50e6867c3b2
Jordan <jlewis2008@live.com>
2020-06-24 20:33:15 -0400

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  dbConnectionString: process.env.DB_CONNECTION_STRING,
+  cookieKey: process.env.COOKIE_KEY,
+};
--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+const dbString = keys.dbConnectionString.replace("<password>", keys.dbPassword);
+mongoose.connect(
+  dbString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB Connection Successful");
+  }
+);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
